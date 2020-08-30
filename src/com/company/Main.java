1a5da71a7df5f24a4b2730cec048c82167294784
TPD <mig.mic@gmail.com>
2020-08-30 14:50:31 +0200

    Extra part done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,57 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         List<Person> workerList = new ArrayList<>();
-        workerList.add(new Administration("Claus","221175-1011",37,24000,5));
-        workerList.add(new Instructor("Tove","011080-1014",20,456));
-        workerList.add(new Administration("Anna","01180-1014",37,23000,5));
-        workerList.add(new Instructor("Henning", "011080-1014",15,456));
+        workerList.add(new Administration("Claus", "221175-1011", 37, 24000, 5));
+        workerList.add(new Instructor("Tove", "011080-1014", 20, 456));
+        workerList.add(new Administration("Anna", "01180-1014", 37, 23000, 5));
+        workerList.add(new Instructor("Henning", "011080-1014", 15, 456));
 
-        List <Person> memberList = new ArrayList<>();
-        memberList.add(new Member("Morten","130195-1303",false));
-        memberList.add(new Member("Martin","221175-1011",true));
-        memberList.add(new Member("Martina","051970-1409",false));
-        memberList.add(new Member("Marcal","221175-1011",true));
+        List<Person> memberList = new ArrayList<>();
+        memberList.add(new Member("Morten", "130195-1303", false));
+        memberList.add(new Member("Martin", "221175-1011", true));
+        memberList.add(new Member("Martina", "051970-1409", false));
+        memberList.add(new Member("Marcal", "221175-1011", true));
 
 
         // Lavede denne liste da jeg ikke kunne bruge .getName og cpr
-        List <Person> fullList = new ArrayList<>();
-        for (int i =0; i < workerList.size();i++) {
-        fullList.add(new Person(workerList.get(i).getName(),workerList.get(i).getCpr())); }
-        for (int j=0; j< memberList.size();j++) {
-            fullList.add(new Person(memberList.get(j).getName(),memberList.get(j).getCpr()));
+        List<Person> fullList = new ArrayList<>();
+        for (int i = 0; i < workerList.size(); i++) {
+            fullList.add(new Person(workerList.get(i).getName(), workerList.get(i).getCpr()));
+        }
+        for (int j = 0; j < memberList.size(); j++) {
+            fullList.add(new Person(memberList.get(j).getName(), memberList.get(j).getCpr()));
         }
 
+        List<Person> fullListFraRead = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(new File("src//resource//persons.txt"));
+            while (sc.hasNextLine()) {
+                fullListFraRead.add(new Person(sc.next(), sc.next()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
 
-    Member n = new Member();
-    Employee m = new Employee();
-    Person a = new Person();
-    m.printOut(workerList);
-    n.printOut(memberList);
-    a.printOuts(fullList);
+        }
+
+        Member n = new Member();
+        Employee m = new Employee();
+        Person a = new Person();
+        m.printOut(workerList);
+        n.printOut(memberList);
+        a.printOuts(fullList);
+        a.printOuts(fullListFraRead);
     }
 }
